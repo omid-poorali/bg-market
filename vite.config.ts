7d5fe85622d5723867070b47b69c96fc39137c94
omid <omidpoorali1992@gmail.com>
2023-12-18 18:14:59 +0330

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,19 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), EnvironmentPlugin('all')],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  }
-})
+  plugins: [react(), tsconfigPaths(), EnvironmentPlugin({
+    // Uses 'development' if the NODE_ENV environment variable is not defined.
+    NODE_ENV: 'development',
+  
+    // Have in mind that variables coming from process.env are always strings.
+    DEBUG: 'false'
+  })],
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   host: true, // needed for the Docker Container port mapping to work
+  //   strictPort: true,
+  //   port: 5173, // you can replace this port with any port
+  // }
+});
